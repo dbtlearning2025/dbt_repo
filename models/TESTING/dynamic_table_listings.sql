1388c69dcfbe57bbdb6f0727ee870ee1469c395f
dbtlearning2025 <dbtlearning2025@gmail.com>
2025-04-29 08:40:53 +0530

    Added how to create secure view and dynamic table

---
+++ models/TESTING/dynamic_table_listings.sql
@@ -0,0 +1,20 @@
+{{
+  config(materialized = 'dynamic_table'
+         ,target_lag = 'downstream'
+         ,snowflake_warehouse ='compute_wh'
+         )
+}}
+WITH src_listings AS 
+( SELECT * FROM DEV.BRONZE_AIRBNB.src_listings)
+SELECT
+           id AS listing_id
+       , name AS listing_name
+       , listing_url
+       , room_type
+       , minimum_nights
+       , host_id
+       , price AS price_str
+       , created_at
+       , updated_at
+FROM
+src_listings
---
+++ models/TESTING/secure_listings_view.sql
@@ -0,0 +1,17 @@
+{{
+  config(materialized = 'view',secure=True)
+}}
+ WITH src_listings AS 
+( SELECT * FROM DEV.BRONZE_AIRBNB.src_listings)
+SELECT
+           id AS listing_id
+       , name AS listing_name
+       , listing_url
+       , room_type
+       , minimum_nights
+       , host_id
+       , price AS price_str
+       , created_at
+       , updated_at
+FROM
+src_listings
